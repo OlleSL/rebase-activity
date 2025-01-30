39f8d53493d34cf16e7f0b49288ed93314bf34d0
christopherbolivar47@gmail.com <frostlord47@DESKTOP-8QKB3A1.>
2025-01-30 16:06:30 -0500

    Fixed issues between feature A and B

--- my_code.py
+++ my_code.py
@@ -1,8 +1,15 @@
-def say_hello(name):
-    print(f"Hello {name}!");
+def say_hello(name, num):
+
+    i = 0
+    while i < num:
+         print(f"hello {name}!")
+         i += 1
+
+say_hello("Walter", 3)
+say_hello("Helen", 2)
+say_hello("Larry", 1)
+say_hello("Ruth", 2)
 
 
-say_hello("Walter")
-say_hello("Ruth")
-say_hello("Larry")
-say_hello("Helen")
+if __name__ == "__main__":
+    say_hello("world", 3);
